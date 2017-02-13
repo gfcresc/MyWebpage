6f6fdc8b175362e361fe8622253cd7f0e44a5708
gfcresc <gfcresc@uw.edu>
2017-02-12 17:10:07 -0800

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,7 @@
+<!-- Basic Js navbar-->
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='maps2.html'>Maps</a></li>" +
+
+"</ul>";
